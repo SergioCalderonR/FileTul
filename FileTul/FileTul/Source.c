b4b46de30ed7031fdaeee3ad32509942f937e6f3
SergioCalderonR <sercal@outlook.com>
2017-09-20 16:30:01 -0500

    -Pictures added

--- FileTul/FileTul/Source.c
+++ FileTul/FileTul/Source.c
@@ -7,6 +7,7 @@ Windows 10 Fall Creators Update
 #include <tchar.h>
 #include <ShlObj.h>
 #include <PathCch.h>
+#include <string.h>
 
 #pragma comment(lib, "Pathcch.lib")
 
@@ -26,76 +27,118 @@ VOID ShowError(DWORD errId)
 
 }
 
+VOID FileCreate(PWSTR path)
+{
+
+	//CreateFileW
+	LPCWSTR fileName = NULL;
+	DWORD desiredAccess = GENERIC_READ | GENERIC_WRITE;
+	DWORD sharedMode = FILE_SHARE_WRITE;
+	DWORD creationDisposition = CREATE_NEW;
+	DWORD flagsAndAttributes = FILE_ATTRIBUTE_NORMAL;
+
+	//PathCchCombine
+	WCHAR pathCombined[MAX_PATH];
+	SIZE_T sizeOfPath = MAX_PATH;
+
+	HANDLE newFile;
+
+	if (PathCchCombine(pathCombined, sizeOfPath, path, L"TestFile.txt") != S_OK)
+		ShowError(GetLastError());
+	else
+	{		
+
+		newFile = CreateFileW(pathCombined, desiredAccess, sharedMode, NULL,
+			creationDisposition, flagsAndAttributes, NULL);
+
+		if (newFile != INVALID_HANDLE_VALUE)
+		{
+			wprintf(L"\nFile was created on: %s\n", pathCombined);
+		}
+		else
+		{
+			wprintf(L"\nFile could not be created. Error code: ");
+			ShowError(GetLastError());
+		}
+	}
+
+
+	
+
+}
+
 int wmain(int argc, WCHAR *argv[])
 {
 	//SHGetKnownFolderPath
 	REFKNOWNFOLDERID folderId = &FOLDERID_Documents;
 	DWORD flags = KF_FLAG_DEFAULT;
 	PWSTR path=NULL;
-
-	//CreateFileW
-	LPCWSTR fileName=NULL;
-	DWORD desiredAccess = GENERIC_READ | GENERIC_WRITE;
-	DWORD sharedMode = FILE_SHARE_WRITE;
-	DWORD creationDisposition = CREATE_NEW;
-	DWORD flagsAndAttributes = FILE_ATTRIBUTE_NORMAL;
-
+	
 	//Results
-	HANDLE newFile;
-	HRESULT hResult;
-
-	//PathCchCombine
-	WCHAR pathCombined[MAX_PATH];
-	SIZE_T sizeOfPath = MAX_PATH;
+	HRESULT hResult;	
 
 	if (argc == 1)
 	{
-		wprintf(L"\nUsage: FileTul [FolderType]");
+		wprintf(L"\nUsage: %s [FolderType]\n", argv[0]);
 		exit(1);
 
 	}
-		
 
-	//Getting the current directory to Documents folder
-	hResult = SHGetKnownFolderPath(folderId, flags, NULL, &path);
-
-	if (hResult != S_OK)
-		wprintf(L"Error code: %lu\n", GetLastError());
-	else //Got the Documents path
+	switch (argc)
 	{
-		wprintf(L"Documents path: %s\n", path);		
-		
-
-		if (PathCchCombine(pathCombined, sizeOfPath, path, L"TestFile.txt") != S_OK)
-			ShowError(GetLastError());
-		else
+	case 2:
+		if (_wcsicmp(argv[1], L"-Documents") == 0)
 		{
+			//Getting the current directory to Documents folder
 			
-			fileName = pathCombined;
-			/*wprintf(L"Full path: %s\n", fileName);*/
+			hResult = SHGetKnownFolderPath(&FOLDERID_Documents, flags, NULL, &path);
 
-			newFile = CreateFileW(fileName, desiredAccess, sharedMode, NULL,
-								creationDisposition, flagsAndAttributes, NULL);
-
-			if (newFile != INVALID_HANDLE_VALUE)
+			if (hResult != S_OK) //If fails
 			{
-				wprintf(L"File was created on: %s\n", fileName);
-			}
-			else
-			{
-				wprintf(L"File could not be created. Error code: ");
+				wprintf(L"Error getting Documents folder path. Code: ");
 				ShowError(GetLastError());
 			}
+			else //Got the Documents path
+			{
+				wprintf(L"\nDocuments path: %s\n", path);
 
-			
+				FileCreate(path);
+				CoTaskMemFree(path);
+				
+			}
 
 		}
+		else if (_wcsicmp(argv[1], L"-Pictures") == 0)
+		{
+			//Getting the current directory to Pictures folder
+
+			hResult = SHGetKnownFolderPath(&FOLDERID_Pictures, flags, NULL, &path);
+
+			if (hResult != S_OK) //If fails
+			{
+				wprintf(L"Error getting Pictures folder path. Code: ");
+				ShowError(GetLastError());
+			}
+			else //Got the Documents path
+			{
+				wprintf(L"\nPictures path: %s\n", path);
+
+				FileCreate(path);
+				CoTaskMemFree(path);
+				
+
+			}
+
 			
+		}
+		else
+			wprintf(L"Miss typing.\n");
 
-		CoTaskMemFree(path);
+		break;
+
+	default:
+		break;
 	}
-
-
 
 	return 0;
 }
